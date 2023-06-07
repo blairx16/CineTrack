4093033f4255bcd7683f3eec624876e7a10f3b8e
blairx16 <135755832+blairx16@users.noreply.github.com>
2023-06-07 13:02:16 +1000

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/movies', (req, res) => {
+  res.json({ message: 'GET request received' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
